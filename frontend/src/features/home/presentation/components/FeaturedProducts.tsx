7fe4383c96b087cc31def712303ddb1a38298013
Mustafa ahmed <56519866+mustafa-ahmed-009@users.noreply.github.com>
2025-03-31 13:51:50 +0200

    chaning the mobile grid size in the featured products feature

--- frontend/src/features/home/presentation/components/FeaturedProducts.tsx
+++ frontend/src/features/home/presentation/components/FeaturedProducts.tsx
@@ -92,7 +92,7 @@ const FeaturedProducts: React.FC = () => {
   );
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-6 my-6">
+    <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-6 my-6">
       {featured.map((product) => (
         <ProductCard product={product} key={product._id} />
       ))}
