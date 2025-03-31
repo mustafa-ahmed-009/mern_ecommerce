6c40b07a2cd1f5dc7dfcbdb6295bd0f5cb21cdef
Mustafa ahmed <56519866+mustafa-ahmed-009@users.noreply.github.com>
2025-03-31 12:08:09 +0200

    setting the domain in production

--- backend/utils/setCookies.js
+++ backend/utils/setCookies.js
@@ -1,10 +1,13 @@
 const setAuthCookie = (res, token) => {
+  const isProduction = process.env.NODE_ENV === 'production';
     res.cookie('token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production', // HTTPS only in production
-      sameSite: 'strict', // Protection against CSRF
-      maxAge:7 * 24 * 60 * 60 * 1000, // Convert days to ms
+      secure: isProduction, // Set Secure flag only in production (HTTPS)
+      sameSite: 'None',     // Necessary for cross-site requests
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
       path: '/',
+      domain: isProduction ? 'ecommerce-tt99-backend.vercel.app' : undefined
+      
     });
 };
 module.exports = setAuthCookie
