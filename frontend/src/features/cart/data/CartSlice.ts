db4bb0f6646b0986412b8127a368341348022c42
Mustafa ahmed <56519866+mustafa-ahmed-009@users.noreply.github.com>
2025-03-31 19:58:12 +0200

    solving adding to the cart error

--- frontend/src/features/cart/data/CartSlice.ts
+++ frontend/src/features/cart/data/CartSlice.ts
@@ -33,11 +33,8 @@ export const cartSlice = createSlice({
       })
       .addCase(CartService.addAnItemToTheCart.fulfilled, (state, action) => {
         state.loading = false;
-        if (state.cart?.cartItems === null) {
-          state.cart.cartItems = []; 
-          state.cart?.cartItems.push(action.payload.data.cartItems); 
-        }
-        state.cart!.cartItems = action.payload.data.cartItems;
+        state.cart = action.payload.data;
+        state.error = null;
       })
       .addCase(CartService.addAnItemToTheCart.rejected, (state, action) => {
         state.loading = false;
